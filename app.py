5e8a71cae4c0e2d667e2200f48e7be3a7d6ccd9c
twvbeek <tim@voxtur.nl>
2023-06-09 09:25:12 +0200

    0.1

--- app.py
+++ app.py
@@ -4,32 +4,41 @@ import openai
 from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
-openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = os.getenv("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")
+
+
+def generate_prompt(job_title, keyword1, keyword2):
+    prompt_template = "We are looking for a {job_title} to join our team. The ideal candidate should have experience in {keyword1} and {keyword2}. {static_sentence}"
+
+    static_sentence = "Plak hier de standaard-zin."  # Replace with your actual sentence
+
+    prompt = prompt_template.format(
+        job_title=job_title,
+        keyword1=keyword1,
+        keyword2=keyword2,
+        static_sentence=static_sentence
+    )
+
+    return prompt
 
 
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        animal = request.form["animal"]
+        job_title = request.form["job_title"]
+        keyword1 = request.form["keyword1"]
+        keyword2 = request.form["keyword2"]
+
         response = openai.Completion.create(
             model="text-davinci-003",
-            prompt=generate_prompt(animal),
+            prompt=generate_prompt(job_title, keyword1, keyword2),
             temperature=0.6,
         )
+
         return redirect(url_for("index", result=response.choices[0].text))
 
     result = request.args.get("result")
     return render_template("index.html", result=result)
 
-
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
-    )
+if __name__ == '__main__':
+    app.run()
---
+++ integration.py
@@ -0,0 +1,47 @@
+import openai
+
+# Set your OpenAI API key
+openai.api_key = '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'
+
+def generate_job_ad(job_name, context_field1, context_field2):
+    # Step 1: Get information from external tool using API call
+    # Replace this with your actual API call to retrieve the job information
+    job_info = get_job_info_from_external_tool()
+
+    # Extract the relevant fields from the job_info
+    job_name = job_info['job_name']
+    context_field1 = job_info['context_field1']
+    context_field2 = job_info['context_field2']
+
+    # Step 2: Send fields to OpenAI API to generate job advert text
+    prompt = f"Job: {job_name}\nContext1: {context_field1}\nContext2: {context_field2}\nGenerate job advert:"
+    job_advert = openai.Completion.create(
+        engine='text-davinci-003',
+        prompt=prompt,
+        max_tokens=100  # Adjust the number of tokens as per your requirements
+    ).choices[0].text.strip()
+
+    # Step 3: Send the generated job advert back to the external tool
+    # Replace this with your actual API call to send the job advert back
+
+    return job_advert
+
+
+# Placeholder function for getting job information from the external tool
+def get_job_info_from_external_tool():
+    # Replace this with your actual API call to retrieve job information
+    job_info = {
+        'job_name': 'Software Developer',
+        'context_field1': 'Experience with Python programming',
+        'context_field2': 'Knowledge of web development frameworks'
+    }
+    return job_info
+
+
+# Example usage
+job_name = "Software Engineer"
+context_field1 = "Experience in cloud computing"
+context_field2 = "Familiarity with machine learning algorithms"
+
+generated_ad = generate_job_ad(job_name, context_field1, context_field2)
+print(generated_ad)
